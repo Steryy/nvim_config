d6f6d65326b71f0daa87ce74910795870357a1dd
Steryyy <dev.8b054@simplelogin.co>
2023-06-09 15:27:09 +0200

    Fix lsp style

--- lua/lazyplugins/lsp.lua
+++ lua/lazyplugins/lsp.lua
@@ -1,39 +1,37 @@
-return{
+return {
+	{
+		"neovim/nvim-lspconfig",
+		cmd = "LspInfo",
 
-  {
-    'neovim/nvim-lspconfig',
-    cmd = 'LspInfo',
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+				cmd={"Mason"},
+			},
 
-
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason-lspconfig.nvim'},
-      {
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-
-      {
-        "j-hui/fidget.nvim",
-        config = true,
-      },
-    },
-    config = function()
-            require("plugins.configs.lspconfig")
-
-    end
-  },
+			{
+				"j-hui/fidget.nvim",
+				config = true,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+		end,
+	},
 
 	{
 		"jose-elias-alvarez/null-ls.nvim",
-        event ="VeryLazy",
-        config = function ()
-            require("plugins.configs.null-ls")
-        end,
-        lazy = true
+		event = "VeryLazy",
+		config = function()
+			require("plugins.configs.null-ls")
+		end,
+		lazy = true,
 	},
-
+	{ "jose-elias-alvarez/typescript.nvim", lazy = true },
 }
